ebc68c8b0675cf6fc53f7efb0238bec0e32d0335
modshehri <mod.shehri@hotmail.com>
2020-10-11 13:38:45 +0300

    Sign in/out

--- public/app.js
+++ public/app.js
@@ -1,1 +1,14 @@
-console.log(firebase)
+const auth = firebase.auth();
+
+const whenSignedIn = document.getElementById('whenSignedIn');
+const whenSignedOut = document.getElementById('wwhenSignedOut');
+
+const signInbtn = document.getElementById('signInBtn');
+const signOutBtn= document.getElementById('signOutBtn');
+
+const userDetails = document.getElementById('userDetails');
+
+const provider = new firebase.auth.GoogleAuthProvider()
+
+signInbtn.onclick = () => auth.signInWithPopup(provider);
+signOutBtn.onclick = () => auth.signOut();
