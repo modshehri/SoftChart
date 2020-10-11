e0f7e44a657d20bf10e59eb4425ba1d8d56ebd13
modshehri <mod.shehri@hotmail.com>
2020-10-11 13:54:30 +0300

    sign in

--- public/app.js
+++ public/app.js
@@ -1,26 +1,29 @@
 const auth = firebase.auth();
 
 const whenSignedIn = document.getElementById('whenSignedIn');
-const whenSignedOut = document.getElementById('wwhenSignedOut');
+const whenSignedOut = document.getElementById('whenSignedOut');
 
-const signInbtn = document.getElementById('signInBtn');
-const signOutBtn= document.getElementById('signOutBtn');
+const signInBtn = document.getElementById('signInBtn');
+const signOutBtn = document.getElementById('signOutBtn');
 
 const userDetails = document.getElementById('userDetails');
 
-const provider = new firebase.auth.GoogleAuthProvider()
 
-signInbtn.onclick = () => auth.signInWithPopup(provider);
+const provider = new firebase.auth.GoogleAuthProvider();
+
+
+signInBtn.onclick = () => auth.signInWithPopup(provider);
+
 signOutBtn.onclick = () => auth.signOut();
 
 auth.onAuthStateChanged(user => {
-    if(user){
-        whenSignedIn.hidden = 'false';
-        whenSignedOut.hidden = 'true';
-        userDetails.innerHTML = '<h3>Hello ${user.displayName}</h3>'
-    }else{
-        whenSignedIn.hidden = 'true';
-        whenSignedOut.hidden = 'false';
-        userDetails.innerHTML = ''
+    if (user) {
+        whenSignedIn.hidden = false;
+        whenSignedOut.hidden = true;
+        userDetails.innerHTML = `<h3>Hello ${user.displayName}!</h3> <p>User ID: ${user.uid}</p>`;
+    } else {
+        whenSignedIn.hidden = true;
+        whenSignedOut.hidden = false;
+        userDetails.innerHTML = '';
     }
-})
+});
