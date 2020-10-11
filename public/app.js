f8460081bf337df5c1e2006564b1f73744460fb4
Abdulelah Hajjar <abdulelahhajjar@gmail.com>
2020-10-11 19:26:29 +0300

    Fix issues

--- public/app.js
+++ public/app.js
@@ -1,20 +1,21 @@
 const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 const whenSignedIn = document.getElementById('whenSignedIn');
 const whenSignedOut = document.getElementById('whenSignedOut');
 
 const signInBtn = document.getElementById('signInBtn');
 const signOutBtn = document.getElementById('signOutBtn');
+const createCanvasBtn = document.getElementById('createCanvasBtn');
 
 const userDetails = document.getElementById('userDetails');
 
-
 const provider = new firebase.auth.GoogleAuthProvider();
 
-
+createCanvasBtn.onclick = () => auth.signOut();
 signInBtn.onclick = () => auth.signInWithPopup(provider);
-
 signOutBtn.onclick = () => auth.signOut();
+
 
 auth.onAuthStateChanged(user => {
     if (user) {
