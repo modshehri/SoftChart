356558756e1060c534bb0f5ff2e3294553b70aaf
Abdulelah Hajjar <abdulelahhajjar@gmail.com>
2020-10-11 23:29:47 +0300

    Implement mouse tracking (local)

--- public/canvas-scripts.js
+++ public/canvas-scripts.js
@@ -9,6 +9,8 @@ const canvasNameDiv = document.getElementById('canvasName');
 const canvasAdminNameDiv = document.getElementById('canvasAdminName');
 const canvasMembersDiv = document.getElementById('canvasMembers');
 
+const mouse = document.getElementById('mouse');
+
 auth.onAuthStateChanged(user => {
     if (!user) {
         window.location.replace("https://softchart-3ee27.web.app/");
@@ -49,3 +51,33 @@ function findGetParameter(parameterName) {
         });
     return result;
 }
+
+(function() {
+    document.onmousemove = handleMouseMove;
+    function handleMouseMove(event) {
+        var eventDoc, doc, body;
+
+        event = event || window.event; // IE-ism
+
+        // If pageX/Y aren't available and clientX/Y are,
+        // calculate pageX/Y - logic taken from jQuery.
+        // (This is to support old IE)
+        if (event.pageX == null && event.clientX != null) {
+            eventDoc = (event.target && event.target.ownerDocument) || document;
+            doc = eventDoc.documentElement;
+            body = eventDoc.body;
+
+            event.pageX = event.clientX +
+              (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+              (doc && doc.clientLeft || body && body.clientLeft || 0);
+            event.pageY = event.clientY +
+              (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
+              (doc && doc.clientTop  || body && body.clientTop  || 0 );
+        }
+
+        // Use event.pageX / event.pageY here
+
+        mouse.style = `display: flexbox; position: absolute; left: ${event.pageX}px; top: ${event.pageY}px; width: 100%; height: 100%;`
+        console.log(event.pageX)
+    }
+})();
