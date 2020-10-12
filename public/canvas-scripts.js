0ae5250902d8b1d49824d3071bebc5ce8cea2188
modshehri <mod.shehri@hotmail.com>
2020-10-12 12:17:46 +0300

    trying

--- public/canvas-scripts.js
+++ public/canvas-scripts.js
@@ -80,7 +80,7 @@ function findGetParameter(parameterName) {
         // Use event.pageX / event.pageY here
 
         mouse.style = `display: flexbox; position: absolute; left: ${event.pageX}px; top: ${event.pageY}px; width: 100%; height: 100%;`
-        userName.innerHTML = '${ documentSnapshot.data().adminUid }'
+        userName.innerHTML = `<h1>Canvas name: ${ documentSnapshot.data().canvasName }</h1>`
         console.log(event.pageX)
     }
 })();
