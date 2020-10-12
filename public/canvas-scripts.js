7c62128f4501b6fd4c13e16de109014dd8c572d1
modshehri <mod.shehri@hotmail.com>
2020-10-12 11:10:05 +0300

    trying

--- public/canvas-scripts.js
+++ public/canvas-scripts.js
@@ -80,7 +80,7 @@ function findGetParameter(parameterName) {
         // Use event.pageX / event.pageY here
 
         mouse.style = `display: flexbox; position: absolute; left: ${event.pageX}px; top: ${event.pageY}px; width: 100%; height: 100%;`
-        userName.innerHTML = '${auth.user.displayName}'
+        userName.innerHTML = auth.user.dislyName
         console.log(event.pageX)
     }
 })();
