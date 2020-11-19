d14ffe0470fbdb41a74d8ccbd858fcc9fa6efa5c
Abdulelah Hajjar <abdulelahhajjar@Abdulelahs-MacBook-Pro.local>
2020-11-19 23:11:56 +0300

    Create "document" UI component; Create "add document" UI component; Add document.svg; Add document-add.svg; Implement loadDocuments in documents.js

--- public/scripts/documents.js
+++ public/scripts/documents.js
@@ -1,15 +1,45 @@
-const auth = firebase.auth();
-const firestore = firebase.firestore();
+const auth = firebase.auth()
+const firestore = firebase.firestore()
 
-const logout = document.getElementById("Logout");
+const logout = document.getElementById("Logout")
+var documentsListener;
 
 auth.onAuthStateChanged(user => {
     if (user) {
-        // TODO:- loadUserData();
-        logout.onclick = () => auth.signOut();
+        loadDocuments(user.uid)
+        logout.onclick = () => auth.signOut()
     } else {
-        auth.signOut();
-        window.location.replace("/index.html");
-        // TODO:- Unsubscribe from all listeners if needed.
+        unsubscribeListeners()
+        clearDocumentsArray()
+        auth.signOut()
+        window.location.replace("/index.html")
     }
-});
+})
+
+function loadDocuments(userId) {
+    documentsListener = firestore.collection('documents')
+        .where('users', 'array-contains', userId)
+        .onSnapshot(querySnapshot => {
+            queryCanvases = querySnapshot.docs.map(doc => {
+                var document = {
+                    id: doc.id,
+                    adminUid: doc.data().adminUid,
+                    name: doc.data().name,
+                    components: doc.data().components,
+                    users: doc.data().users
+                }
+
+                console.log(document)
+            })
+        })
+}
+
+function unsubscribeListeners() {
+    if (documentsListener != null) {
+        documentsListener.unsubscribe()
+    }
+}
+
+function clearDocumentsArray() {
+    documents = []
+}
