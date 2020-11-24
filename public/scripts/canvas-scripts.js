5aecbc1465ee1893b84d13c239ae7190c40fe4df
Abdulelah Hajjar <abdulelahhajjar@Abdulelahs-MacBook-Pro.local>
2020-11-24 19:09:32 +0300

    Implement user management dialog in canvas.html; Implement shadow effect covering the canvas under user management dialog; Implemented simple animations when showing or hiding the dialog

--- public/scripts/canvas-scripts.js
+++ public/scripts/canvas-scripts.js
@@ -1,4 +1,25 @@
 const myDocs = document.getElementById("DocsButton");
+const umcButton = document.getElementById("user-management-button");
+const canvasShadowDiv = document.getElementById("canvas-shadow");
+const userManagementDiv = document.getElementById("user-management");
+
 myDocs.onclick = function () {
     location.href = "documents.html";
 };
+
+umcButton.onclick = function() {
+    $(`#${canvasShadowDiv.id}`).css({"z-index": "1"});
+    $(`#${userManagementDiv.id}`).css({"z-index": "1"});
+    $(`#${canvasShadowDiv.id}`).fadeIn('slow');
+    $(`#${userManagementDiv.id}`).fadeIn('slow');
+}
+
+canvasShadowDiv.onclick = function() {
+    $(`#${canvasShadowDiv.id}`).fadeOut('slow', function() {
+        $(`#${canvasShadowDiv.id}`).css({"z-index": "0"});
+    });
+
+    $(`#${userManagementDiv.id}`).fadeOut('slow', function() {
+        $(`#${userManagementDiv.id}`).css({"z-index": "0"});
+    });
+}
